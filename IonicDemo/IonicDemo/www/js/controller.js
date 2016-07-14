b38d3989b312dd6a49cf1b313f10ba766f29b4f1
ChaohuiZhang <herochao001@163.com>
2016-07-14 10:28:46 +0800

    add router and slide page

---
+++ IonicDemo/IonicDemo/www/js/controller.js
@@ -0,0 +1,25 @@
+angular.module('home.controllers', [])
+
+    .controller('NewsCtrl', function ($scope) {
+        $scope.options = {
+            loop: true,
+            effect: 'fade',
+            speed: 500,
+        }
+
+        $scope.$on("$ionicSlides.sliderInitialized", function (event, data) {
+            // data.slider is the instance of Swiper
+            $scope.slider = data.slider;
+        });
+
+        $scope.$on("$ionicSlides.slideChangeStart", function (event, data) {
+            console.log('Slide change is beginning');
+        });
+
+        $scope.$on("$ionicSlides.slideChangeEnd", function (event, data) {
+            // note: the indexes are 0-based
+            $scope.activeIndex = data.activeIndex;
+            $scope.previousIndex = data.previousIndex;
+        });
+    });
+
